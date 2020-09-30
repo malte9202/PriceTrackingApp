2e8ff4196127d46ae148292aa73b604b6135a7e3
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-09-30 20:44:28 +0200

    ref

--- Database.py
+++ Database.py
@@ -14,3 +14,24 @@ class Database(object):
         )
         # set db cursor with buffered parameter
         self.db_cursor = self.db_connection.cursor(buffered=True)
+
+    def execute_migration(self, migrations):
+        self.db_cursor.execute('CREATE TABLE IF NOT EXISTS migrations '
+                               '(id INT AUTO_INCREMENT PRIMARY KEY, '
+                               'name VARCHAR(255), '
+                               'executed BOOLEAN DEFAULT FALSE);')
+        for migration in migrations:
+            executed_migrations_raw = self.execute_query('SELECT name FROM migrations WHERE executed = 1;')
+            executed_migrations = []
+            for executed_migration in executed_migrations_raw:
+                executed_migrations.append(executed_migration[0])
+            if migration not in executed_migrations:
+                self.db_cursor.execute('INSERT INTO migrations (name) VALUES (%s);', (migration,))
+                self.db_cursor.execute(migrations[migration])
+                self.db_connection.commit()
+                self.db_cursor.execute('UPDATE migrations '
+                                       'SET executed = 1'
+                                       'WHERE name = (%s);', (migration,))
+                self.db_connection.commit()
+            else:
+                pass
