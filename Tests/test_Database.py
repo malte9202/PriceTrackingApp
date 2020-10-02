2eec759d83ae3a508ddfbc3ef65a7711cbc04e3c
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-10-02 22:33:02 +0200

    raw unittests for database

---
+++ Tests/test_Database.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+
+class TestDatabase(TestCase):
+    def test_execute_migration(self):
+        self.fail()
+
+    def test_insert_product(self):
+        self.fail()
+
+    def test_insert_price(self):
+        self.fail()
+
+    def test_get_url(self):
+        self.fail()
+
+    def test_get_product_ids(self):
+        self.fail()
+
+    def test_execute_query(self):
+        self.fail()
+
+    def test_delete_product(self):
+        self.fail()
+
+    def test_delete_price(self):
+        self.fail()
