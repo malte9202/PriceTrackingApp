ba964638199070f6aab11e5401d036c2e0febdb2
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-10-02 22:44:06 +0200

    main file comments

--- Main.py
+++ Main.py
@@ -8,7 +8,9 @@ database = Database()  # create db connection
 
 product_ids = Database.get_product_ids(database)  # fetch all product_ids from database
 
-for product_id in product_ids:
+for product_id in product_ids:  # for each product
+    # get url and price
     url = Database.get_url(database, product_id)
     price = Scraper.scrape_price(url)
+    # db insert
     Database.insert_price(database, product_id, price)
