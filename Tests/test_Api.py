c09ca897e7b376bd780b65174db31c33014505fb
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-10-02 22:37:32 +0200

    raw unittests for Api

---
+++ Tests/test_Api.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+
+class TestProductList(TestCase):
+    def test_get(self):
+        self.fail()
+
+
+class TestCreateProduct(TestCase):
+    def test_post(self):
+        self.fail()
+
+
+class TestProduct(TestCase):
+    def test_get(self):
+        self.fail()
+
+    def test_delete(self):
+        self.fail()
+
+
+class TestPrice(TestCase):
+    def test_get(self):
+        self.fail()
