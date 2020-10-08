3527a1fd3af61d050ea19873620eb1682048aa8a
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-10-08 21:15:51 +0200

    fixed unittest

--- Migrations.py
+++ Migrations.py
@@ -6,7 +6,8 @@ def migrate() -> None:
     migrations = {
         'migration_0001': 'CREATE TABLE IF NOT EXISTS products '
                           '(id INT AUTO_INCREMENT PRIMARY KEY, '
-                          'name VARCHAR(255), '
+                          'name VARCHAR(255),'
+                          'price_threshold FLOAT, '
                           'url VARCHAR(255));',
         'migration_0002': 'CREATE TABLE IF NOT EXISTS prices '
                           '(product_id INT NOT NULL, '
--- Tests/test_Database.py
+++ Tests/test_Database.py
@@ -1,27 +1,36 @@
-from unittest import TestCase
+from unittest import TestCase  # import unittest functions
+from Database import Database  # import database class
 
 
 class TestDatabase(TestCase):
     def test_execute_migration(self):
-        self.fail()
+        pass
 
-    def test_insert_product(self):
-        self.fail()
+    def test_insert_product(self) -> None:
+        test_database = Database()
+        # insert test product
+        Database.insert_product(test_database, 'test product', 99.99, 'https://testproducturl.com')
+        test_query = 'SELECT url FROM products WHERE name = \'test product\';'
+        # check if url is equal
+        self.assertEqual([('https://testproducturl.com',)], Database.execute_query(test_database, test_query))
+        # get id and delete the test product from database
+        test_id = Database.execute_query(test_database, 'SELECT id FROM products WHERE name = \'test product\';')[0][0]
+        Database.delete_product(test_database, test_id)
 
     def test_insert_price(self):
-        self.fail()
+        pass
 
     def test_get_url(self):
-        self.fail()
+        pass
 
     def test_get_product_ids(self):
-        self.fail()
+        pass
 
     def test_execute_query(self):
-        self.fail()
+        pass
 
     def test_delete_product(self):
-        self.fail()
+        pass
 
     def test_delete_price(self):
-        self.fail()
+        pass
