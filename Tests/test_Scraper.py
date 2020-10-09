0b26cf3d3989f8a1998183775b6d39057136fa5a
Malte Niedereichholz <malte@Maltes-Air.fritz.box>
2020-10-09 22:53:08 +0200

    adjusted scraper test

--- Tests/test_Scraper.py
+++ Tests/test_Scraper.py
@@ -6,16 +6,16 @@ class TestScraper(TestCase):
     def test_scrape_price(self):
         self.assertEqual(
             type(Scraper.scrape_price('https://geizhals.de/hisense-55a7100f-a2286242.html?hloc=at&hloc=de')),
-            float)
+            float, msg='price should be float')
         self.assertEqual(
             type(Scraper.scrape_price('https://geizhals.de/gigabyte-g27qc-a2304341.html?hloc=at&hloc=de')),
-            float)
+            float, msg='price should be float')
         self.assertEqual(
             type(Scraper.scrape_price('https://geizhals.de/apple-iphone-se-2020-64gb-schwarz-a2273374.html')),
-            float)
+            float, msg='price should be float')
         self.assertEqual(
             type(Scraper.scrape_price('https://geizhals.de/apple-macbook-air-space-gray-mwtj2d-a-a2255044.html')),
-            float)
+            float, msg='price should be float')
         self.assertEqual(
             type(Scraper.scrape_price('https://geizhals.de/apple-ipad-10-2-128gb-mw772fd-a-mw772ll-a-a2132800.html')),
-            float)
+            float, msg='price should be float')
